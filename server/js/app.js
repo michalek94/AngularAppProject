8563874ab1fb43d1b89c48df8f8760144d9a6135
michalek94 <pankos30@gmail.com>
2016-05-24 17:04:00 +0200

    Added index.html

---
+++ server/js/app.js
@@ -0,0 +1,28 @@
+angular.module('myapp', ['ui.router']).config(function ($stateProvider, $urlRouterProvider) {
+    //
+    // For any unmatched url, redirect to /main
+    $urlRouterProvider.otherwise("/main");
+    //
+    // Now set up the states
+    $stateProvider
+    .state('main', {
+        url: "/main",
+        controller: "MainCtrl",
+        templateUrl: "partials/main.html"
+    })
+    .state('order', {
+        url: "/order",
+        controller: "OrderCtrl",
+        templateUrl: "partials/order.html"
+    })
+    .state('status', {
+        url: "/status/:orderId",
+        controller: "StatuCtrl",
+        templateUrl: "partials/status.html"        
+    })
+    .state('contact', {
+        url: "contact",
+        controller: "ContactCtrl",
+        templateUrl: "partials/contact.html"
+    })
+});
