606d586398de0f69c75c1ff6ad851f80be4f54e0
Grzegorz Bartczak <gwmagazynier@gmail.com>
2016-05-29 23:22:00 +0200

    app.js

--- server/js/app.js
+++ server/js/app.js
@@ -8,21 +8,21 @@ angular.module('myapp', ['ui.router']).config(function ($stateProvider, $urlRout
     .state('main', {
         url: "/main",
         controller: "MainCtrl",
-        templateUrl: "parts/main.html"
+        templateUrl: "partials/main.html"
     })
     .state('order', {
         url: "/order",
         controller: "OrderCtrl",
-        templateUrl: "parts/order.html"
+        templateUrl: "partials//order.html"
     })
     .state('status', {
         url: "/status/:orderId",
         controller: "StatuCtrl",
-        templateUrl: "parts/status.html"        
+        templateUrl: "partials/status.html"        
     })
     .state('contact', {
         url: "contact",
         controller: "ContactCtrl",
-        templateUrl: "parts/contact.html"
+        templateUrl: "partials/contact.html"
     })
 });
